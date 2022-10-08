42400e7aaba64c3e6ac8f8f56f01cc045c72ae33
emilkjuka <emilkjuka@gmail.com>
2022-10-08 20:40:56 +0200

    Implementing random buttons to test out functions

--- blog/src/components/Card.svelte
+++ blog/src/components/Card.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
     export let contents: String ;
 
+
+    function click() {
+        console.log("clicked");
+        
+    }
 </script>
 
 <div class=" p-1 h-48 w-full rounded-xl bg-orange-400 m-2 ">
     <h1>{contents}</h1>
+    <button class="bg-white text-gray-600 font-sans rounded-xl w-24" on:click="{click}">Click me!</button>
 </div>
 
 <style>
