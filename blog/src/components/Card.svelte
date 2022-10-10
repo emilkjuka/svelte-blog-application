b60ed8b2d3d57b9fc1fd50aa05fb16fffc5ced45
emilkjuka <emilkjuka@gmail.com>
2022-10-10 22:51:51 +0200

    Skeleton for new post

--- blog/src/components/Card.svelte
+++ blog/src/components/Card.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 </script>
 
-<div class=" p-1 h-48 w-[768px] rounded-xl bg-orange-400 m-2 ">
+<div class=" p-1 h-48 w-[768px] rounded-xl bg-orange-400 m-2 p-4">
    <slot></slot>
 </div>
 
--- blog/src/components/NewPost.svelte
+++ blog/src/components/NewPost.svelte
@@ -3,5 +3,11 @@
 </script>
 
 <Card>
-    <p>New Post Placeholder</p>
+    <div class="flex justify-between">
+        <input class="" placeholder="Title" />
+        <button class="bg-white text-gray-600 rounded-md">Submit</button>
+    </div>
+    <div class="py-2 w-full">
+        <textarea class="rounded-md" rows="5" cols="98" placeholder="Enter your post here!" />
+    </div>
 </Card>
